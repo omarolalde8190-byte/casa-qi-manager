c602369b3015e4ceb0363595f2185358aae07ec7
omarolalde8190-byte <omarolalde8190@gmail.com>
2026-07-02 19:11:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-.DS_Store
-Thumbs.db
-*.tmp
-*.log
-node_modules/
-.env
+window.CASA_QI_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwCubqVLMh5ncNbN9Dh64z3xU6_oT2zzs9w-m3gZgX519l7e7tVB4u9kNwMSZ1INqKP/exec";
